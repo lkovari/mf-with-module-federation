1f2ae2393b1ed33dc2852a89f146ed28de74e3f3
lkovari <laszlo.kovary@gmail.com>
2025-05-11 21:38:07 +0200

    build scripts, manifest and webpack config

--- micro-fe-workspace/projects/shell/webpack.prod.config.js
+++ micro-fe-workspace/projects/shell/webpack.prod.config.js
@@ -1,1 +1,11 @@
-module.exports = require('./webpack.config');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    "mf-a": "mf-a@https://lkovari.github.io/mf-microfrontend/mf-a/remoteEntry.js",
+    "mf-b": "mf-b@https://lkovari.github.io/mf-microfrontend/mf-b/remoteEntry.js",
+  },
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+});
