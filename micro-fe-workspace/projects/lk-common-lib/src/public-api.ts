9721b559bac92830e9364d891b793fdbe84e367a
lkovari <laszlo.kovary@gmail.com>
2025-05-18 13:39:53 +0200

    remove all

--- micro-fe-workspace/projects/lk-common-lib/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of lk-common-lib
- */
-
-export * from './lib/lk-common-lib.service';
-export * from './lib/lk-common-lib.component';
--- micro-fe-workspace/projects/mf-a/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { MFA_Routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(MFA_Routes)]
-};
