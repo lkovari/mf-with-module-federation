ce872778fa8c1114b9f029533470ca4460d43c8c
lkovari <laszlo.kovary@gmail.com>
2025-05-18 20:20:59 +0200

    It works locally anf remotely

--- mfworkspace/projects/shell/src/main.ts
+++ mfworkspace/projects/shell/src/main.ts
@@ -1,7 +1,13 @@
 import { initFederation } from '@angular-architects/module-federation';
+import { environment } from './environments/environment';
 
+const isDev = location.hostname === 'localhost'
 
-initFederation('mf.manifest.json')
+const manifestUrl = isDev
+  ? '/mf.manifest.dev.json'
+  : '/mf-with-module-federation/mf.manifest.prod.json';
+
+initFederation(manifestUrl)
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
